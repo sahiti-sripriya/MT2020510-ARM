9d5793659cc2e3d20989bc6112ed198cefdb3e48
sahiti-sripriya <88818817+sahiti-sripriya@users.noreply.github.com>
2021-10-31 20:44:27 +0530

    Update fib.s

--- assign_three/fib.s
+++ assign_three/fib.s
@@ -1,4 +1,4 @@
-     AREA     arreven, CODE, READONLY
+          AREA     arreven, CODE, READONLY
      EXPORT __main
      IMPORT printMsg
 	 IMPORT printMsg2p
@@ -7,17 +7,17 @@
 __main  FUNCTION	
 		MOV R0, #0x20000000
 		MOV R1, #0x0
-		STR R1, [R0]
+		STR R1, [R0, #0x5]
 		MOV R2, #0x1
-	    STR R2, [R0, #0x1]
-		MOV R3, #0x2
+	    STR R2, [R0, #0xA]
+		MOV R3, #0xF
 LOOPA
 		ADD R4,R1,R2
 		STR R4, [R0,R3]
 		MOV R1,R2
 		MOV R2,R4
-		ADD R3,R3,#0x1
-		CMP R3,#0x20
+		ADD R3,R3,#0x5
+		CMP R3,#0x64
 		IT LE
 		BLE LOOPA
 stop    B stop ; stop program
